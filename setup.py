ace3695e2edfcba571ebf7e6f6da6c8b194dd4bb
Mohammad Hadi Hosseinpour <wpparsi@gmail.com>
2025-04-17 20:36:34 +0330

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zarf",
-    version="0.0.1",
+    version="0.0.2",
     description="A simple Dependency Container library",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
